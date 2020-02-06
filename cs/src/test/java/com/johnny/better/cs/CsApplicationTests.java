1b450075ae6d36b11f74baa2cc2c275bf9702654
ndgndg91 <ndgndg91@gmail.com>
2020-02-06 22:47:55 +0900

    <chore> project init

---
+++ cs/src/test/java/com/johnny/better/cs/CsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.better.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
